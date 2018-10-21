d10e6f3304ad968279caacad98a41423172a8340
Patrick Gehrke <39940651+patrickgehrke@users.noreply.github.com>
2018-10-21 15:38:33 +0200

    ADD: unhandled exception handling in app.xaml.cs
    log UnobservedTaskException, DispatcherUnhandledException and CurrentDomain.UnhandledException

--- src/WpfPlayground/App.xaml.cs
+++ src/WpfPlayground/App.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +10,41 @@ namespace WpfPlayground
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            InstallExceptionHandling();
+        }
+
+        private void InstallExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+                LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+                LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            string msg;
+
+            try
+            {
+                msg = $"[ERROR] Unhandled exception in {source}";
+                AssemblyName assembly = Assembly.GetExecutingAssembly().GetName();
+                msg = $"[ERROR] Unhandled exception in {assembly.Name} version: {assembly.Version}";
+            }
+            catch (Exception ex)
+            {
+                //Add Log
+            }
+
+            //Add Log
+        }
     }
 }
