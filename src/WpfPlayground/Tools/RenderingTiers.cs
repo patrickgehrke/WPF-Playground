b5d3ee596bd76f7e0f726b319546676e2b9b0a94
Patrick Gehrke <39940651+patrickgehrke@users.noreply.github.com>
2018-10-21 15:19:07 +0200

    ADD: directx rendering tiers check

---
+++ src/WpfPlayground/Tools/RenderingTiers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfPlayground.Tools
+{
+    public class RenderingTiers
+    {
+        private int renderingTier;
+
+        public RenderingTiers()
+        {
+            this.renderingTier = RenderCapability.Tier >> 16;
+        }
+        
+        public void ShowTier()
+        {
+            string msg = "";
+
+            switch(this.renderingTier)
+            {
+                case 0:
+                    msg = "No graphics hardware acceleration. All graphics features use software acceleration. The DirectX version level is less than version 9.0.";
+                    break;
+
+                case 1:
+                    msg = "Some graphics features use graphics hardware acceleration. The DirectX version level is greater than or equal to version 9.0. ";
+                    break;
+
+                case 2:
+                    msg = "Most graphics features use graphics hardware acceleration. The DirectX version level is greater than or equal to version 9.0. ";
+                    break;
+            }
+
+            MessageBox.Show($"Tier = {this.renderingTier}{Environment.NewLine}{msg}");
+        }
+    }
+}
