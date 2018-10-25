c40b22ace4013a5b855b41112fbde1cdc4d94c3c
Patrick Gehrke <39940651+patrickgehrke@users.noreply.github.com>
2018-10-25 21:09:03 +0200

    ADD: cm logging based on type

--- src/WpfPlayground.Cm/Bootstrapper.cs
+++ src/WpfPlayground.Cm/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using System;
 using System.Windows;
 using WpfPlayground.Cm.ViewModels;
 
@@ -9,8 +10,62 @@ namespace WpfPlayground.Cm
         public Bootstrapper()
         {
             base.Initialize();
+            LogManager.GetLog = (Type t) => this.CaliburnLogging(t);
         }
 
+        /// <summary>
+        ///     Logging based on type
+        /// </summary>
+        /// <param name="t">Caliburn Type</param>
+        /// <returns>Logging Instance</returns>
+        private ILog CaliburnLogging(Type t)
+        {
+            ILog log = new DebugLogging();
+
+            switch(t.ToString())
+            {
+                case nameof(Coroutine):
+                    break;
+                case nameof(OverrideCancelResultDecorator):
+                    break;
+
+                case nameof(ContinueResultDecorator):
+                    break;
+
+                case nameof(RescueResultDecorator<Exception>):
+                    break;
+
+                case nameof(Screen):
+                    break;
+
+                case nameof(Caliburn.Micro.Action):
+                    break;
+
+                case nameof(Bind):
+                    break;
+
+                case nameof(Parser):
+                    break;
+
+                case nameof(View):
+                    break;
+
+                case nameof(ViewModelBinder):
+                    break;
+
+                case nameof(ActionMessage):
+                    break;
+
+                case nameof(ViewLocator):
+                    break;
+
+                case nameof(ConventionManager):
+                    break;
+            }
+
+            return log;
+        }
+        
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             base.OnStartup(sender, e);
---
+++ src/WpfPlayground.Cm/DebugLogging.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using Caliburn.Micro;
+
+namespace WpfPlayground.Cm
+{
+    public class DebugLogging : ILog
+    {
+        public void Error(Exception exception)
+        {
+            Debug.WriteLine("[ERROR] " + exception.Message);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            Debug.WriteLine("[INFO] " + String.Format(format, args));
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            Debug.WriteLine("[WARN] " + String.Format(format, args));
+        }
+    }
+}
