9e3d5745d30f993a001224d6eac62c1fccf08e02
Patrick Gehrke <39940651+patrickgehrke@users.noreply.github.com>
2018-11-02 18:59:34 +0100

    ADD: doevents dispatcher

---
+++ src/WpfPlayground.Miscellaneous/DoEvents.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Windows.Threading;
+
+namespace WpfPlayground.Miscellaneous
+{
+    public class DoEvents
+    {
+        /*
+         https://kent-boogaart.com/blog/wpf-splash-screens
+        */
+        public void Do(DispatcherPriority dispatcherPriority = DispatcherPriority.Loaded)
+        {
+            var dispatcherFrame = new DispatcherFrame();
+            Dispatcher.CurrentDispatcher.BeginInvoke((ThreadStart)(() => dispatcherFrame.Continue = false), dispatcherPriority);
+            Dispatcher.PushFrame(dispatcherFrame);
+        }
+    }
+}
